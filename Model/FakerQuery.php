c1935d81acc92e23912ca9093129962fbdef7861
Alberttyty <thierry@pixel-plurimedia.fr>
2021-02-04 09:14:34 +0100

    faker

--- Model/FakerQuery.php
+++ Model/FakerQuery.php
@@ -67,3 +67,237 @@ $moviesByCategory = [
         $fullMetalJacket,
     ],
 ];
+
+$pseMovies = [
+    1 => [
+        'movie' => $pathOfGlory,
+        'combinations' => [
+            1 => [
+                'price' => 10.000000,
+                'title' => 1
+            ]
+            2 => [
+                'price' => 15.000000,
+                'title' => 5
+            ],
+            3 => [
+                'price' => 18.000000,
+                'title' => 10
+            ],
+            5 => [
+                'price' => 20.000000,
+                'title' => 20
+            ],
+        ]
+    ],
+    2 => [
+        'movie' => $spartacus,
+        'combinations' => [
+            1 => [
+                'price' => 10.000000,
+                'title' => 1
+            ]
+            2 => [
+                'price' => 15.000000,
+                'title' => 5
+            ],
+            3 => [
+                'price' => 18.000000,
+                'title' => 10
+            ],
+            5 => [
+                'price' => 20.000000,
+                'title' => 20
+            ],
+        ]
+    ],
+    3 => [
+        'movie' => $lolita,
+        'combinations' => [
+            1 => [
+                'price' => 10.000000,
+                'title' => 1
+            ]
+            2 => [
+                'price' => 15.000000,
+                'title' => 5
+            ],
+            3 => [
+                'price' => 18.000000,
+                'title' => 10
+            ],
+            5 => [
+                'price' => 20.000000,
+                'title' => 20
+            ],
+        ]
+    ],
+    4 => [
+        'movie' => $drStrangeLove,
+        'combinations' => [
+            1 => [
+                'price' => 11.500000,
+                'title' => 1
+            ]
+            2 => [
+                'price' => 13.000000,
+                'title' => 5
+            ],
+            3 => [
+                'price' => 16.000000,
+                'title' => 10
+            ],
+            5 => [
+                'price' => 20.000000,
+                'title' => 20
+            ],
+        ]
+    ],
+    5 => [
+        'movie' => $aSpaceOdyssey,
+        'combinations' => [
+            1 => [
+                'price' => 10.000000,
+                'title' => 1
+            ]
+            2 => [
+                'price' => 15.000000,
+                'title' => 5
+            ],
+            3 => [
+                'price' => 18.000000,
+                'title' => 10
+            ],
+            5 => [
+                'price' => 20.000000,
+                'title' => 20
+            ],
+        ]
+    ],
+    6 => [
+        'movie' => $clockworkOrange,
+        'combinations' => [
+            1 => [
+                'price' => 10.500000,
+                'title' => 1
+            ]
+            2 => [
+                'price' => 12.000000,
+                'title' => 5
+            ],
+            3 => [
+                'price' => 15.000000,
+                'title' => 10
+            ],
+            5 => [
+                'price' => 18.000000,
+                'title' => 20
+            ],
+        ]
+    ],
+    7 => [
+        'movie' => $barryLindon,
+        'combinations' => [
+            1 => [
+                'price' => 7.500000,
+                'title' => 1
+            ]
+            2 => [
+                'price' => 10.000000,
+                'title' => 5
+            ],
+            3 => [
+                'price' => 12.500000,
+                'title' => 10
+            ],
+            5 => [
+                'price' => 15.000000,
+                'title' => 20
+            ],
+        ]
+    ],
+    8 => [
+        'movie' => $shining,
+        'combinations' => [
+            1 => [
+                'price' => 12.000000,
+                'title' => 1
+            ]
+            2 => [
+                'price' => 13.500000,
+                'title' => 5
+            ],
+            3 => [
+                'price' => 15.500000,
+                'title' => 10
+            ],
+            5 => [
+                'price' => 18.000000,
+                'title' => 20
+            ],
+        ]
+    ],
+    9 => [
+        'movie' => $fullMetalJacket,
+        'combinations' => [
+            1 => [
+                'price' => 12.500000,
+                'title' => 1
+            ]
+            2 => [
+                'price' => 16.000000,
+                'title' => 5
+            ],
+            3 => [
+                'price' => 18.500000,
+                'title' => 10
+            ],
+            5 => [
+                'price' => 21.000000,
+                'title' => 20
+            ],
+        ]
+    ],
+    10 => [
+        'movie' => $eyesWideShut,
+        'combinations' => [
+            1 => [
+                'price' => 10.500000,
+                'title' => 1
+            ]
+            2 => [
+                'price' => 15.500000,
+                'title' => 5
+            ],
+            3 => [
+                'price' => 18.000000,
+                'title' => 10
+            ],
+            5 => [
+                'price' => 19.500000,
+                'title' => 20
+            ],
+        ]
+    ],
+    11 => [
+        'movie' => $dune,
+        'combinations' => [
+            1 => [
+                'price' => 12.000000,
+                'title' => 1
+            ]
+            2 => [
+                'price' => 13.000000,
+                'title' => 5
+            ],
+            3 => [
+                'price' => 15.000000,
+                'title' => 10
+            ],
+            5 => [
+                'price' => 18.000000,
+                'title' => 20
+            ],
+        ]
+    ],
+];
--- cart.php
+++ cart.php
@@ -25,3 +25,27 @@ if (! empty($_POST['product_id']) && ! empty($_POST['quantity'])
     echo('</body>'.PHP_EOL);
     echo('</html>'.PHP_EOL);
 }
+
+if (! empty($_POST['product_id']) && ! empty($_POST['combination_id'])
+    //&& isset($_SERVER['HTTP_X_REQUESTED_WITH'])
+    //&& strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest'
+) {
+    header('Content-Type: text/html; charset=utf-8');
+
+    echo('<!DOCTYPE html>'.PHP_EOL);
+    echo('<html>'.PHP_EOL);
+    echo('<head>'.PHP_EOL);
+    echo('<meta charset="utf-8" />'.PHP_EOL);
+    echo('<title>Page panier</title>'.PHP_EOL);
+    echo('</head>'.PHP_EOL);
+    echo('<body>'.PHP_EOL);
+    echo('<h1>Welcome to the Movie API</h1>'.PHP_EOL);
+    echo('<p>Please, send a POST "product_id" and "combination_id".</p>'.PHP_EOL);
+    echo('<div id="cart-container">'.PHP_EOL);
+    echo('<p>'.PHP_EOL);
+    echo($pseMovies[$_POST['product_id']]['movie']->title.' => '.$pseMovies[$_POST['product_id']]['combinations'][$_POST['combination_id']]['price'].' € <br />added to cart'.PHP_EOL);
+    echo('</p>'.PHP_EOL);
+    echo('</div>'.PHP_EOL);
+    echo('</body>'.PHP_EOL);
+    echo('</html>'.PHP_EOL);
+}
